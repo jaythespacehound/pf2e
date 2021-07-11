d8ccea48d0bb469bf535e89b8fa434b81d2ffa7e
Jay Blanchard <jaythespacehound@gmail.com>
2021-07-11 14:06:12 -0600

    First start of char class

--- src/pf2e/charClass.py
+++ src/pf2e/charClass.py
@@ -1,3 +1,64 @@
+class Skills:
+    """Base skills in the game"""
+    def __init__(self):
+        self.acrobatics = 0
+        self.arcana = 0
+        self.athletics = 0
+        self.crafting = 0
+        self.deception = 0
+        self.diplomacy = 0
+        self.intimidation = 0
+        self.lores = {}
+        self.medicine = 0
+        self.nature = 0
+        self.occultism = 0
+        self.performance = 0
+        self.religion = 0
+        self.society = 0
+        self.stealth = 0
+        self.survival = 0
+        self.thievery = 0
+        
+    def skill_names(self):
+        """Returns all the names above (variables in this class)"""
+        return(vars(self))
+    
+    def __str__(self):
+        """Tells python print how to format these data"""
+        return("\n".join("{:12} {}".format(k, v) for k, v in self.skill_names().items()))
+    
+    def update_skills(self, char):
+        """Do the maths here to update the skills depending on attributes and proficiencies"""
+
+
+
+class CharacterPathfinder2e:
+    """ Base char class - all characters (PCs and NPCs/Monsters) have these traits:"""
+    def __init__(self, name):
+        self.name = name
+        self.level = 0
+        self.hp = 0
+        self.ac = 0
+        self.fortitude = 0
+        self.reflex = 0
+        self.will = 0
+        self.perception = 0
+        self.speed = 0
+        self.size = "medium"
+        self.skills = Skills() #should maybe move to the playerClass
+        
+
+#This will move outside this file but for testing
+aChar = CharacterPathfinder2e("Test Char")
+
+print("Char name, willpower:")
+print(aChar.name, aChar.will)
+print()
+print("Skills:")
+print(aChar.skills.skill_names())
+print(aChar.skills)
+
+
 ##not starting with a PLAYER character: better to have scalability than not
 ##class CharacterPathfinderSecondEdition - wow, 
 ##    - all characters (PCs and NPCs/Monsters) have these traits:
